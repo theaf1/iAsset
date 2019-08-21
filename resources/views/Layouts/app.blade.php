c58fa10b9863963c7598fda2d29f81d1d4d04a20
= <=>
2019-08-21 10:54:36 +0700

    app templates

--- resources/views/Layouts/app.blade.php
+++ resources/views/Layouts/app.blade.php
@@ -1,2 +1,10 @@
 <html>
+<head>
+    <title>ระบบติดตามครุภัณฑ์คอมพิวเตอร์ - @yield('title')</title>
+    <link rel="stylesheet" href="{{ url('/css/bootstrap.css') }}">
+</head>
+<body>
+    <div class="container">
+    @yield('content')
+</body>
 </html>
