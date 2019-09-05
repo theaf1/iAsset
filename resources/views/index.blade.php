93effa1b0453ab34c0366be5991552dd4768c13f
= <=>
2019-09-05 12:02:17 +0700

    ทำ form หยาบๆ

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -10,8 +10,8 @@
     <option value="Notebook">Notebook</option>
     <option value="allinone">All-In-One</option>
 </select>
-<p>รหัส SAP</p><input name="sapid" type="text">
-<p>รหัสครุภัณฑ์</p><input name="pid" type="text">
+<p>รหัส SAP<input name="sapid" type="text"></p>
+<p>รหัสครุภัณฑ์<input name="pid" type="text"></p>
 <p>ตึก</p>
 <select name="location">
     <option value="location">location</option>
@@ -24,7 +24,7 @@
     <option value="section">section</option>
 </select>
 <p>ชื่อผู้ใช้งาน</p><input name="user" type="text">
-<p>xitหน่วยงาน</p>
+<p>ประเภทหน่วยงาน</p>
 <select name="section_status">
     <option value="สำนักงาน">สำนักงาน</option>
     <option value="หอผู้ป่วย">หอผู้ป่วย</option>
@@ -47,7 +47,63 @@
     <option value="status3">ส่งคืนแล้ว</option>
 </select>
 <p>หมายเหตุ</p>
-<textarea name="remarks" rows="5" columns="180">
-</textarea>
+<textarea name="remarks" rows="5" columns="180"></textarea>
+<p>ยี่ห้อ<input name="brand" type="text"></p>
+<p>รุ่น<input name="model" type="text"></p>
+<p>Serial Number<input name="serial_no" type="text"></p>
+<p>CPU Model<input name="cpu_model" type="text"></p>
+<p>CPU Speed<input name="cpu_speed" type="text"></p>
+<p>CPU Socket Number<input name="cpu_socket_no" type="number" min="1" value="1"></p>
+<p>RAM Size<input name="ram_size" type="number" min="0"></p>
+็<p>HDD number<input name="hdd_no" type="number" min="1" value="1"></p>
+<p>HDD type</p>
+<p>HDD Size</p>
+<p>จำนวนจอที่ใช้งาน<input name="display_no" type="number" min="0" value="1"></p>
+<p>SAP จอ<input name="display_sapid" type="text"></p>
+<p>รหัสครุภัณฑ์จอภาพ<input name="display_pid" type="text"></p>
+<p>ขนาดจอภาพ<input name="display_size" type="number"></p>
+<p>สัดส่วนจอภาพ<input name="display_ratio" type="text" pattern="{0-9}:{0-9}"></p>
+<p>OS<input name="os" type="text"></p>
+<p>OS architecture</p>
+<input type="radio" name="os_arch" value="yes">32 bit<br>
+<input type="radio" name="os_arch" value="no">64 bit<br>
+<p>Microsoft Office Version<input name="ms_office_ver" type="text"></p>
+<p>Antivirus<input name="antivirus" type="text"></p>
+<p>PDF reader<input name="pdf_reader" type="text"></p>
+<p>IE version<input name="ie_version" type="number" value="6"></p>
+<p>Chrome version<input name="chrome_version" type="text"></p>
+<p>SPSS version<input name="spss_version" type="number" value="17"></p>
+<p>E-HIS</p>
+<input type="radio" name="ehis" value="yes">มี<br>
+<input type="radio" name="ehis" value="no">ไม่มี<br>
+<p>SiPACS</p>
+<input type="radio" name="sipac" value="yes">มี<br>
+<input type="radio" name="sipac" value="no">ไม่มี<br>
+<p>i-scan</p>
+<input type="radio" name="si_iscan" value="yes">มี<br>
+<input type="radio" name="si_iscan" value="no">ไม่มี<br>
+<p>ECLAIR</p>
+<input type="radio" name="eclair" value="yes">มี<br>
+<input type="radio" name="eclair" value="no">ไม่มี<br>
+<p>Admission Notes</p>
+<input type="radio" name="admission_note" value="yes">มี<br>
+<input type="radio" name="admission_note" value="no">ไม่มี<br>
+<p>SiNET</p>
+<input type="radio" name="sinet" value="yes">มี<br>
+<input type="radio" name="sinet" value="no">ไม่มี<br>
+<p>Software อื่นๆ</p>
+<input type="checkbox" name="other_software" value="yes">มี Softwareอื่นๆ<br>
+<textarea name="other_software_detail" rows="5" columns="180"></textarea>
+<p>LAN outlet No<input name="lan_outlet_no" type="text"></p>
+<p>IP Address<input name="ip_addresss" type="text"></p>
+<p>MAC Address<input name="mac_addresss" type="text"></p>
+<p>ประเภทเครือข่าย</p>
+<input type="radio" name="lan_type" value="0">ไม่เชื่อมต่อ<br>
+<input type="radio" name="lan_type" value="1">MUCNET<br>
+<input type="radio" name="lan_type" value="2">Internet โรงพยาบาล<br>
+<input type="radio" name="lan_type" value="3">ระบบภายในโรงพยาบาล<br>
+<p>Computer Name<input name="computer_name" type="text"></p>
+<p>ปัญหาในการใช้งาน</p>
+<textarea name="issues" rows="5" columns="180"></textarea>
 </form>
 @endsection 
